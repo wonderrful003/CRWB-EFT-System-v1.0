105d9f7ef29b505dafd283d6e3c9e811f4d3698c
Wonderful Ashley Ntepa <wonderfulntepa3@gmail.com>
2026-02-03 12:58:28 +0200

    modified:   backup.json 	modified:   crwb_eft/settings.py

--- crwb_eft/settings.py
+++ crwb_eft/settings.py
@@ -61,12 +61,8 @@ WSGI_APPLICATION = 'crwb_eft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crwb_eft_system',
-        'USER': 'eft_user',
-        'PASSWORD': 'SecurePassword123!',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
